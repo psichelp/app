3412b6629f95e281714d1248ef3b3966ab2bd0e8
Evandro Junior <evandrojr@gmail.com>
2018-09-09 08:09:11 -0300

    fontes material design instalado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule, MatIconRegistry } from '@angular/material';
 import { MatGridListModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
@@ -14,6 +14,7 @@ import { ListPageComponent } from './list-page/list-page.component';
 import { LocalService } from './services/local/local.service';
 import { PipesModule } from './pipes/pipes.module';
 import { DetailsPageComponent } from './details-page/details-page.component';
+import { HttpClientModule } from '@angular/common/http';
 
 const appRoutes: Routes = [
   { path: 'inicio', component: HomePageComponent},
@@ -43,8 +44,15 @@ const appRoutes: Routes = [
     MatListModule,
     MatCardModule,
     MatGridListModule,
+    HttpClientModule,
     PipesModule.forRoot(),
   ], providers: [LocalService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(matIconRegistry: MatIconRegistry, domSanitizer: DomSanitizer){
+    matIconRegistry.addSvgIconSet(domSanitizer.bypassSecurityTrustResourceUrl('./assets/mdi.svg')); // Or whatever path you placed mdi.svg at
+  }
+
+ }
