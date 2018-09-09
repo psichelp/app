c7e17d2dd248694f8bad228698b841aa5cea2f1c
Evandro Junior <evandrojr@gmail.com>
2018-09-09 07:37:10 -0300

    fazendo a pagina de detalhes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule } from '@angular/material';
 import { MatGridListModule } from '@angular/material';
 
-
-
 import { AppComponent } from './app.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -15,13 +13,13 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { ListPageComponent } from './list-page/list-page.component';
 import { LocalService } from './services/local/local.service';
 import { PipesModule } from './pipes/pipes.module';
-
+import { DetailsPageComponent } from './details-page/details-page.component';
 
 const appRoutes: Routes = [
   { path: 'inicio', component: HomePageComponent},
   { path: '',   redirectTo: '/inicio', pathMatch: 'full' },
   { path: 'listar', component: ListPageComponent},
-  { path: 'third-page', component: AppComponent}
+  { path: 'detalhes', component: DetailsPageComponent}
 ];
 
 @NgModule({
@@ -29,7 +27,8 @@ const appRoutes: Routes = [
     AppComponent,
     SidebarComponent,
     HomePageComponent,
-    ListPageComponent
+    ListPageComponent,
+    DetailsPageComponent
   ],
   imports: [
     BrowserModule,
