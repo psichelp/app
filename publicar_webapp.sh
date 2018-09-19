a12d4a7258614ef4fc27a845be3463ffde6cb3d9
Evandro Junior <evandrojr@gmail.com>
2018-09-19 20:50:12 -0300

    Publicando o webApp no http://psichelp.github.io/app/webapp

--- publicar_webapp.sh
+++ publicar_webapp.sh
@@ -8,14 +8,14 @@ APP="PsicHelp"
     git push
 }
 
-ng build --prod --base-href /webapp/
+ng build --prod
 rm -rf docs
-mkdir -p docs/webapp
-cp dist/PsicHelp/* docs/webapp -r
+mkdir -p docs
+cp dist/PsicHelp/* docs -r
 cd docs
-google-chrome http://0.0.0.0:8015/webapp/
+google-chrome http://0.0.0.0:8015
 php -S 0.0.0.0:8015
-echo Teste primeiro localmente, funcionou? http://0.0.0.0:8015/webapp/
+echo Teste primeiro localmente, funcionou? http://0.0.0.0:8015/
 read answer
 if [ "$answer" != "${answer#[SsYy]}" ] ;then
     echo OK, continuando
@@ -26,13 +26,12 @@ fi
 
 cd ..
 acp Publicando o webApp no http://psichelp.github.io/app/webapp 
-cd ../site
 
+cd ../site
 git checkout master
 git pull origin master
 rm -rf ios
 rm -rf webapp
-
 cp ../app/dist/PsicHelp webapp -r
 
 acp Publicando o webApp no psichelp.com.br/webapp 
