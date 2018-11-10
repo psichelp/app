308ff3efc7862eb4c046f049e01ebbdd6e7d02a2
Evandro Junior <evandrojr@gmail.com>
2018-11-10 16:19:32 -0300

    Enviando notificação de cadastro

--- src/app/pages/cadastro-entidade/cadastro-entidade.component.ts
+++ src/app/pages/cadastro-entidade/cadastro-entidade.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormBuilder, FormArray, Validators } from '@angular/forms';
 import { DengodbService } from 'src/app/services/dengodb/dengodb.service';
 import { SERVICO } from 'src/app/services/local/tipos-de-servico';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from "rxjs/operators";
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-cadastro-entidade',
@@ -11,11 +16,10 @@ import { SERVICO } from 'src/app/services/local/tipos-de-servico';
 export class CadastroEntidadeComponent implements OnInit {
   
   control: FormControl;
-  constructor(private fb: FormBuilder, private dengodb: DengodbService) {
-    // this.control=this.entidadeForm;
-  }
-
-  servicosTipos = Object.values(SERVICO);
+  constructor(private fb: FormBuilder, private dengodb: DengodbService, private router: Router, private http: HttpClient) {}
+  telegramUrl = 'https://api.telegram.org/bot747846139:AAEpVYndvdgt6pQRcNyGex7A283hg3qlk0c/sendMessage?chat_id=569816047&text=';
+  
+  servicosTipos = Object.values(SERVICO).sort();
 
   entidadeForm = this.fb.group({
     nome: ['', Validators.required],
@@ -27,7 +31,7 @@ export class CadastroEntidadeComponent implements OnInit {
     servicos: this.fb.array([
       this.fb.control('')
     ]),
-    descricao_servicos:[''],
+    descricao_servicos: [''],
     mapa: [''],
     video: [''],
     bairros_atendidos: [''],
@@ -35,11 +39,9 @@ export class CadastroEntidadeComponent implements OnInit {
     crm: [''],
   });
 
-  ngOnInit() {
+  ngOnInit() { }
 
-  }
-
-  cadastrar(){
+  cadastrar() {
     console.log(this.entidadeForm.value);
   }
 
@@ -51,30 +53,31 @@ export class CadastroEntidadeComponent implements OnInit {
     this.servicos.push(this.fb.control(''));
   }
 
-  // get aliases() {
-  //   return this.profileForm.get('aliases') as FormArray;
-  // }
 
-  // updateProfile() {
-  //   this.profileForm.patchValue({
-  //     firstName: 'Nancy',
-  //     address: {
-  //       street: '123 Drew Street'
-  //     }
-  //   });
-  // }
-
-  // addAlias() {
-  //   this.aliases.push(this.fb.control(''));
-  // }
+  telegramMessage(mensagem): Observable<any> {
+    mensagem = encodeURI(mensagem);
+    let apiUrl = `${this.telegramUrl}${mensagem}`;
+    return this.http.get(apiUrl)
+      .pipe(map(res => {
+        let results = res;
+        return results;
+      }));
+  }
 
   solicitarCadastro() {
-    // TODO: Use EventEmitter with form value
     console.warn(this.entidadeForm.value);
     let entidade = this.entidadeForm.value;
     entidade.ativo = false;
     let r = this.dengodb.insert(entidade, 'test');
     console.log(r);
-
+    this.telegramMessage("Novo cadastro " + JSON.stringify(entidade, null, '  ') ).subscribe(data => {
+      console.log('Mensagem de cadastro enviada com sucesso', data);
+    },
+      error => {
+        console.error('Erro ao enviar mensagem de cadastro', error);
+      }
+    );
+    alert("Muito obrigado! Cadastro efetuado com sucesso");
+    this.router.navigate(['/'])
   }
 }
