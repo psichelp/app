c62b1d838557160995764bb41d2f8f0a3a0e34df
Evandro Junior <evandrojr@gmail.com>
2018-11-10 15:15:50 -0300

    bonito mas não funciona

--- src/app/pages/cadastro-entidade/cadastro-entidade.component.ts
+++ src/app/pages/cadastro-entidade/cadastro-entidade.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormBuilder, FormArray, Validators } from '@angular/forms';
+import { DengodbService } from 'src/app/services/dengodb/dengodb.service';
+import { SERVICO } from 'src/app/services/local/tipos-de-servico';
 
 @Component({
   selector: 'app-cadastro-entidade',
@@ -7,16 +9,25 @@ import { FormControl, FormBuilder, FormArray, Validators } from '@angular/forms'
   styleUrls: ['./cadastro-entidade.component.css']
 })
 export class CadastroEntidadeComponent implements OnInit {
+  
+  control: FormControl;
+  constructor(private fb: FormBuilder, private dengodb: DengodbService) {
+    // this.control=this.entidadeForm;
+  }
 
-  profileForm = this.fb.group({
+  servicosTipos = Object.values(SERVICO);
+
+  entidadeForm = this.fb.group({
     nome: ['', Validators.required],
     end: [''],
     tel: [''],
     whatsapp: [''],
-    email: [''],
+    email: ['', Validators.email],
     valor: [''],
-    servicos: [''],
-    //   descricao_servicos:[''],
+    servicos: this.fb.array([
+      this.fb.control('')
+    ]),
+    descricao_servicos:[''],
     mapa: [''],
     video: [''],
     bairros_atendidos: [''],
@@ -28,28 +39,42 @@ export class CadastroEntidadeComponent implements OnInit {
 
   }
 
-  get aliases() {
-    return this.profileForm.get('aliases') as FormArray;
+  cadastrar(){
+    console.log(this.entidadeForm.value);
   }
 
-  constructor(private fb: FormBuilder) { }
-
-
-  updateProfile() {
-    this.profileForm.patchValue({
-      firstName: 'Nancy',
-      address: {
-        street: '123 Drew Street'
-      }
-    });
+  get servicos() {
+    return this.entidadeForm.get('servicos') as FormArray;
   }
 
-  addAlias() {
-    this.aliases.push(this.fb.control(''));
+  addServico() {
+    this.servicos.push(this.fb.control(''));
   }
 
-  onSubmit() {
+  // get aliases() {
+  //   return this.profileForm.get('aliases') as FormArray;
+  // }
+
+  // updateProfile() {
+  //   this.profileForm.patchValue({
+  //     firstName: 'Nancy',
+  //     address: {
+  //       street: '123 Drew Street'
+  //     }
+  //   });
+  // }
+
+  // addAlias() {
+  //   this.aliases.push(this.fb.control(''));
+  // }
+
+  solicitarCadastro() {
     // TODO: Use EventEmitter with form value
-    console.warn(this.profileForm.value);
+    console.warn(this.entidadeForm.value);
+    let entidade = this.entidadeForm.value;
+    entidade.ativo = false;
+    let r = this.dengodb.insert(entidade, 'test');
+    console.log(r);
+
   }
 }
---
+++ src/app/services/dengodb/dengodb.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Base64 } from 'js-base64';
+import { GithubService } from '../github/github.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DengodbService {
+
+  constructor(private github: GithubService) { }
+
+  public insert(value, file) {
+    this.github.get(`/src/assets/data/${file}.json`).subscribe(data => {
+      console.log('rawData', data);
+      console.log("Conteúdo antes", Base64.decode((data.content)));
+      let contents = JSON.parse(Base64.decode((data.content)));
+      contents.push(value);
+      console.log("Conteúdo depois", contents);
+      this.github.update(`/src/assets/data/${file}.json`, JSON.stringify(contents, null, "  "), data.sha)
+    });
+  }
+
+}
