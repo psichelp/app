f4c1ca8a321d88ddeea955debb83a824e0b044d3
Evandro Leite Jr <evandro.leite@serpro.gov.br>
2021-04-07 10:28:55 -0300

    Criado o TelegramService

---
+++ src/app/services/telegram/telegram.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TelegramService {
+
+  constructor(private http: HttpClient) { }
+
+  telegramUrl = 'https://api.telegram.org/bot747846139:AAES926AKuygvC5QYC8-AuqQdJIUsBWdTlE/sendMessage?chat_id=569816047&text=';
+
+  send(mensagem): Observable<any> {
+    console.log(mensagem);
+    mensagem = encodeURI(mensagem);
+
+    let apiUrl = `${this.telegramUrl}${mensagem}`;
+    return this.http.get(apiUrl)
+      .pipe(map(res => {
+        let results = res;
+        console.log(res);
+        return results;
+      }));
+  }
+
+}
